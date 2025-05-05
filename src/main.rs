0b876e334917a74bff0514a343ae2d4faf0e0aee
Seth Bashford <seth.bashford@boystown.org>
2025-05-05 11:00:34 -0500

    improvements

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,73 @@ use clap::Parser;
 use std::{io::Write, process::ExitStatus};
 
 #[derive(Parser)]
-#[command(version = option_env!("IHN_HPC_SBATCH_ARRAY_VERSION").unwrap_or("debug"))]
+#[command(version = option_env!("IHN_HPC_SBATCH_ARRAY_VERSION").unwrap_or("debug"), verbatim_doc_comment)]
+/// A program wrapping Slurm's "sbatch" command tailored for IHN's HPC cluster.
+///
+/// ihn-hpc-sbatch-array wraps Slurm's "sbatch" command and creates a job array
+/// from a text file. The text file (COMMAND_ARG_PATH) contains one nonempty line
+/// for each job in the array. Each line is trimmed and passed to COMMAND as the
+/// one and only argument. The COMMAND is executed inside a podman container. The
+/// user's home directory (/mnt/home/username/) and shared directory
+/// (/mnt/home/shared/) are mounted inside the container at the same locations.
+///
+/// EXAMPLE
+/// Given the file "arg.txt" with contents:
+/// M68123456
+/// M68654321
+/// M68112233
+///
+/// and file "script.sh" with contents:
+/// #!/bin/bash
+/// SUBJECT=$1
+/// HOME=/mnt/home/username/
+/// recon-all -i "$HOME"/"$SUBJECT".nii -sd "$HOME"/subjects/ -subjid "$SUBJECT" -all
+///
+/// The command:
+/// $ ihn-hpc-sbatch-array freesurfer /path/to/script.sh /path/to/arg.txt
+///
+/// creates a three element job array each running a freesurfer container that
+/// executes "script.sh". In this case, the script receives an argument
+/// representing the subject ID and calls "recon-all" for the given subject.
 struct Args {
-    #[arg(long)]
-    image: Option<String>,
-    #[arg(long)]
-    max_tasks: Option<String>,
-    #[arg(long)]
+    /// The maximum number of simultaneous tasks
+    #[arg(long, default_value_t = 16)]
+    max_tasks: i32,
+    /// Additional args to sbatch
+    #[arg(long, allow_hyphen_values = true)]
     sbatch_args: Option<String>,
-    #[arg(long)]
+    /// Additional args to podman
+    #[arg(long, allow_hyphen_values = true)]
     podman_args: Option<String>,
-    container: Container,
+    /// Podman image - short-hand identifier or qualified name
+    ///
+    /// IMAGE specifies the podman image used for each container. A short-hand identifier, e.g.
+    /// "freesurfer", may be used for known images. Otherwise IMAGE is passed directly to
+    /// podman-run for each job.
+    #[arg(value_parser = parse_image)]
+    image: Image,
+    /// Command to execute inside each container
+    ///
+    /// COMMAND specifies the command executed inside each container. If COMMAND has a shell script
+    /// extension (.sh) and exists on the host it is treated as a user-defined shell script and
+    /// mounted inside each container.
     command: String,
+    /// Path to a plaintext file containing one argument per line - the one
+    /// argument passed to COMMAND for each array job
     command_arg_path: std::path::PathBuf,
 }
 
-#[derive(clap::ValueEnum, Clone, Copy)]
-enum Container {
+#[derive(Clone)]
+enum Image {
     Freesurfer,
-    Other,
+    QualifiedName(String),
+}
+
+fn parse_image(s: &str) -> anyhow::Result<Image> {
+    match s.to_lowercase().as_str() {
+        "freesurfer" => Ok(Image::Freesurfer),
+        _ => Ok(Image::QualifiedName(s.to_string())),
+    }
 }
 
 fn main() -> std::process::ExitCode {
@@ -57,26 +105,26 @@ fn run() -> anyhow::Result<ExitStatus> {
     let (command, command_volume_arg) = {
         let command_path = std::path::Path::new(&args.command);
         if command_path.exists() && command_path.extension().is_some_and(|ext| ext == "sh") {
-            let mounted_command_path = format!("/{}", args.command);
+            let mounted_command_path = std::fs::canonicalize(command_path).with_context(||
+                format!("It looks like \"{command}\" is a shell script, but a canonical path cannot be determined for mounting in each container", command=args.command)
+            )?.to_str().with_context(||format!("\"{command}\" is not valid UTF-8", command=args.command))?.to_string();
             (
                 mounted_command_path.clone(),
-                format!("-v {}:{}", args.command, mounted_command_path),
+                format!(
+                    "-v {}:{}",
+                    mounted_command_path.clone(),
+                    mounted_command_path
+                ),
             )
         } else {
             (args.command, "".to_string())
         }
     };
-    let image = match container_image(args.container) {
-        Some(image) => image.to_string(),
-        None => args
-            .image
-            .context("--image must be specified if \"other\" container is chosen")?,
-    };
     let mut sbatch = std::process::Command::new("sbatch")
         .arg(format!(
             "--array=0-{}%{}",
             command_arg_vec.len() - 1,
-            args.max_tasks.unwrap_or("16".to_string())
+            args.max_tasks
         ))
         .args(args.sbatch_args)
         .stdin(std::process::Stdio::piped())
@@ -89,18 +137,21 @@ fn run() -> anyhow::Result<ExitStatus> {
 set -u
 export TMPDIR=/ssd/home/$USER/TEMP
 export REGISTRY_AUTH_FILE=/mnt/apps/etc/auth.json
-INPUT=(
-{input_array}
+COMMAND_ARGS=(
+{command_args}
 )
 srun --ntasks=1 podman run --rm \
+    -v \"$HOME\":\"$HOME\" \
+    -v /mnt/home/shared/:/mnt/home/shared/ \
     {command_volume_arg} \
-    {volume_args} \
+    {container_volume_args} \
     --entrypoint {command} \
     {podman_args} \
-    {image} \"${{INPUT[$SLURM_ARRAY_TASK_ID]}}\"",
-            input_array = command_arg_vec.join("\n"),
-            volume_args = volume_args_for_container(args.container),
+    {image} \"${{COMMAND_ARGS[$SLURM_ARRAY_TASK_ID]}}\"",
+            command_args = command_arg_vec.join("\n"),
+            container_volume_args = volume_args_for_container(&args.image),
             podman_args = args.podman_args.unwrap_or("".to_string()),
+            image = qualified_image_name(&args.image)
         )?;
     } else {
         return Err(anyhow!("Unable to take stdin of sbatch"));
@@ -108,19 +159,20 @@ srun --ntasks=1 podman run --rm \
     Ok(sbatch.wait()?)
 }
 
-fn volume_args_for_container(c: Container) -> &'static str {
+fn volume_args_for_container(c: &Image) -> &'static str {
     match c {
-        Container::Freesurfer => {
-            "-v /mnt/apps/etc/fs_license.txt:/usr/local/freesurfer/.license:ro \
+        Image::Freesurfer => {
+            "\
+-v /mnt/apps/etc/fs_license.txt:/usr/local/freesurfer/.license:ro \
 -v /opt/matlab/runtime/R2019b/v97/:/usr/local/freesurfer/MCRv97"
         }
-        Container::Other => "",
+        Image::QualifiedName(_) => "",
     }
 }
 
-fn container_image(c: Container) -> Option<&'static str> {
+fn qualified_image_name(c: &Image) -> &str {
     match c {
-        Container::Freesurfer => Some("freesurfer/freesurfer:7.3.2"),
-        Container::Other => None,
+        Image::Freesurfer => "freesurfer/freesurfer:7.3.2",
+        Image::QualifiedName(n) => n,
     }
 }
